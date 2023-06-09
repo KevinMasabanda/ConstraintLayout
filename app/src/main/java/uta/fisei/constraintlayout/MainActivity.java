1ae5978ea942ae1d30beba9abc775a37c4e209a6
unknown <kevinmasabanda2c@gmail.com>
2023-06-09 10:31:23 -0500

    EJEMPLO1

---
+++ app/src/main/java/uta/fisei/constraintlayout/MainActivity.java
@@ -0,0 +1,20 @@
+package uta.fisei.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickButtonLogin(View view){
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
